97f550bc3c05a002a687631d634a6f1bd53a3610
vuduc153 <vuductran153@gmail.com>
2024-07-14 17:38:51 +1000

    Remapped axes/buttons to match tiago control

--- scripts/joystick.js
+++ scripts/joystick.js
@@ -30,14 +30,34 @@ function initJoystick() {
         const gamepad = navigator.getGamepads()[gamepad_idx];
 
         let message = new ROSLIB.Message({
-            axes: suppressAxisDrift(gamepad.axes),
-            buttons: gamepad.buttons.map((x) => { return x.pressed ? 1 : 0; }),
+            axes: suppressAxisDrift(remapAxes(gamepad.axes)),
+            buttons: remapButtons(gamepad.buttons)
         });
         joyTopic.publish(message);
     }
 
+    function remapAxes(axes) {
+        return axes.map((x) => -x);
+    }
+
+    function remapButtons(buttons) {
+        const pressed = buttons.map((x) => { return x.pressed ? 1 : 0; });
+        let newButtons = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        newButtons[0] = pressed[3];
+        newButtons[1] = pressed[0];
+        newButtons[2] = pressed[1];
+        newButtons[3] = pressed[2];
+        newButtons[4] = pressed[12];
+        newButtons[6] = pressed[13];
+        newButtons[5] = pressed[14];
+        newButtons[7] = pressed[15];
+        newButtons[8] = pressed[8];
+        newButtons[9] = pressed[9];
+        return newButtons;
+    }
+
     function suppressAxisDrift(axes) {
-        return axes.map(item => Math.abs(item) < 0.3 ? 0: item);
+        return axes.map(item => Math.abs(item) < 0.1 ? 0: item);
     }
 
     setInterval(publishGamepadState, 100 /* 10 Hz */);
